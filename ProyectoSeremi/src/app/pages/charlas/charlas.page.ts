9522a2b6d96fce7d67f1ac77de792d2064396f39
LeandroChamorro <leandro.chamorro.r@mail.pucv.cl>
2025-05-06 00:08:53 -0400

    Elementos de charla
    Se añaden botones laterales a charla-específica, entre ellos el contador de tiempo.

--- ProyectoSeremi/src/app/pages/charlas/charlas.page.ts
+++ ProyectoSeremi/src/app/pages/charlas/charlas.page.ts
@@ -20,5 +20,7 @@ export class CharlasPage implements OnInit {
   goToCharla() {
     this.router.navigate(['/charla-especifica']);
   }
-
+  goToMenu() {
+    this.router.navigate(['/principal']);
+  }
 }
